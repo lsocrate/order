72f4768028ab6d07ee9dd3e0c166991ea581b093
Luiz Sócrate <luiz@socrate.com.br>
2013-07-25 13:10:31 -0300

    calculate ranking

---
+++ calc.php
@@ -0,0 +1,26 @@
+<?php
+$log = 'options.txt';
+$record = fopen($log, 'r');
+$text = trim(fread($record, 9999999));
+$votes = explode("\n", $text);
+$ranking = array();
+
+foreach ($votes as $json) {
+    $vote = json_decode($json);
+    foreach ($vote as $voter => $list) {
+        $rank = explode(',', $list);
+        $list_size = count($rank);
+        foreach ($rank as $key => $value) {
+            if (!isset($ranking[$value])) {
+                $ranking[$value] += 0;
+            }
+            $ranking[$value] += ($list_size - $key);
+        }
+    }
+}
+
+uasort($ranking, function($a, $b){
+    return $a - $b;
+});
+
+var_dump($ranking);
